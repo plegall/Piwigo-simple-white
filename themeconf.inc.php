e45d7e92ce1b50601543121944c0822b2454a77e
plegall <plg@piwigo.org>
2010-06-01 23:32:54 +0000

    simple-white theme for Piwigo 2.1 and higher
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple-white@6442 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Theme Name: Simple White
+Version: 2.1.a
+Description: Theme white for Simple
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=346
+Author: plg
+Author URI: http://piwigo.wordpress.com
+*/
+$themeconf = array(
+  'name'  => 'simple-white',
+  'parent' => 'simple',
+  );
+?>
